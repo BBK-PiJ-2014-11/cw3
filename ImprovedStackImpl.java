4fc24dbfc00ac12689c66a041d7aa4a4bd9f96b1
Ehshan <eveerabangsa@gmail.com>
2015-02-01 12:06:57 +0000

    fixed bug in top() + pop() + added JavaDocs

--- ImprovedStackImpl.java
+++ ImprovedStackImpl.java
@@ -1,71 +1,93 @@
 /**
- * Created by Ehshan
-*/
-
+ * {@inheritDoc}
+ *
+ * @author Ehshan Veerabangsa
+ */
 public class ImprovedStackImpl implements ImprovedStack {
-    // list ok? or should it be stack? - check
-    private List improvedStack;
+    private List stackList;
 
+    /**
+     * ImprovedStackImpl class constructor
+     *
+     * @param list - the internal list  - The data structure where the elements on the stack are stored.
+     */
     public ImprovedStackImpl(List list){
-        improvedStack = list;
+        stackList = list;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public ImprovedStack reverse() {
-        ImprovedStack reverseStack = new ImprovedStackImpl(new ArrayList());
+        ImprovedStack reverseStack = new ImprovedStackImpl(new LinkedList());
         if (isEmpty()) {
             return reverseStack;
         }else {
             for (int i = 0; i < size(); i++) {
-                reverseStack.push(improvedStack.get(i).getReturnValue());
+                reverseStack.push(stackList.get(i).getReturnValue());
             }
         }
         return reverseStack;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void remove(Object object) {
-        //is get() + remove() allowed??? - check
         for (int i=0; i< size(); i++) {
-            if (improvedStack.get(i).getReturnValue().equals(object)) {
-                improvedStack.remove(i);
+            if (stackList.get(i).getReturnValue().equals(object)) {
+                stackList.remove(i);
             }
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isEmpty() {
-        return improvedStack.isEmpty();
+        return stackList.isEmpty();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int size() {
-        return improvedStack.size();
+        return stackList.size();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void push(Object item) {
-
-        improvedStack.add(item);
-
+        stackList.add(item);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public ReturnObject top() {
-
         if (isEmpty()) {
             return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
         } else {
-            return improvedStack.get(0);
+            return stackList.get(size() - 1);
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public ReturnObject pop() {
-
         if (isEmpty()) {
             return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
         } else {
-            return improvedStack.remove(0);
+            return stackList.remove(size()-1);
         }
 
     }
