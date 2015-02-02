b482e439610513644d2af3c997607786e39d81d9
Ehshan <eveerabangsa@gmail.com>
2015-02-02 06:11:16 +0000

    moved data structures to separate package

--- Structures/ReturnObjectImpl.java
+++ Structures/ReturnObjectImpl.java
@@ -1,22 +1,40 @@
-package structures;
+package Structures;
 
 /**
- * Implementation of ReturnObject. Returns either an object or an error
+ * {@inheritDoc}
+ *
+ * @author Ehshan Veerabangsa
  */
 public class ReturnObjectImpl implements ReturnObject {
+
     private Object returnObject;
     private ErrorMessage error;
 
+
+    /**
+     * ReturnObjectImpl class constructor. An object will be returned if the operation was successful
+     *
+     * @param returnObject
+     */
     public ReturnObjectImpl(Object returnObject) {
         this.returnObject = returnObject;
         this.error = ErrorMessage.NO_ERROR;
     }
 
+    /**
+     * ReturnObjectImpl class constructor. An error message will be returned if the operation was unsuccessful
+     *
+     * @param error a pre defined error message
+     */
     public ReturnObjectImpl(ErrorMessage error) {
         this.returnObject = null;
         this.error = error;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public boolean hasError() {
         if (error != ErrorMessage.NO_ERROR){
             return true;
@@ -25,6 +43,10 @@ public class ReturnObjectImpl implements ReturnObject {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public ErrorMessage getError() {
         if (hasError()) {
             return error;
@@ -33,6 +55,10 @@ public class ReturnObjectImpl implements ReturnObject {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public Object getReturnValue() {
         if (hasError()) {
             return error;
